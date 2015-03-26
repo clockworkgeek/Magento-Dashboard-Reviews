e8a3067be99ee85dd782979badde53c73a61b888
Daniel Deady <daniel@clockworkgeek.com>
2015-03-26 20:05:48 +0000

    Show review count in tab title
    Thanks to "arrudatr" for suggestion

--- code/Block/Tab.php
+++ code/Block/Tab.php
@@ -23,13 +23,15 @@ class Clockworkgeek_DashboardReviews_Block_Tab extends Mage_Adminhtml_Block_Abst
         /* @var $session Mage_Admin_Model_Session */
         $session = Mage::getSingleton('admin/session');
         if ($session->isAllowed('catalog/reviews_ratings/reviews/pending')) {
+            /* @var $reviews Clockworkgeek_DashboardReviews_Model_Resource_Review_Pending_Collection */
+            $reviews = Mage::getModel('dashboardreviews/resource_review_pending_collection');
             /* @var $grids Mage_Adminhtml_Block_Dashboard_Grids */
             $grids = $this->getLayout()
                 ->getBlock('content')
                 ->getChild('dashboard')
                 ->getChild('grids');
             $grids->addTab('pending_reviews', array(
-                'label' => Mage::helper('review')->__('Pending Reviews'),
+                'label' => Mage::helper('review')->__('Pending Reviews (%d)', $reviews->getSize()),
                 'url' => $this->getUrl('*/dashboardReviews/pending', array(
                     '_current' => true
                 )),
